f54b662ae84616a6cde15f4e738b221e0b62a1c2
Levi Arcane <zarcanist95@gmail.com>
2023-02-16 00:53:51 -0500

    Journal Entry Schema completed

---
+++ models/Entry.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const entrySchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Entry', entrySchema);
