37c9fc0db1c86f2bdb29e20be83d66547f7942b2
Levi Arcane <zarcanist95@gmail.com>
2023-02-16 00:49:15 -0500

    User Schema complete

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    memberSince: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.pre('save', function(next) {
+    let user = this;
+    if(!user.isModified('password')) return next();
+    
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(user.password, salt);
+    user.password = hash;
+    next();
+});
+
+userSchema.methods.withoutPassword = function(){
+    const user = this.toObject();
+    delete user.password;
+    return user;
+}
+
+userSchema.methods.comparePassword = function(enteredPassword, callback){
+    bcrypt.compare(enteredPassword, this.password, (err, isMatch) => {
+        if(err) return callback(err);
+        callback(null, isMatch);
+    });
+}
+
+module.exports = mongoose.model('User', userSchema);
